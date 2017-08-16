5ae388c05971f60d92eba23de7b80c8f955fb4fd
Tim Freeman <d.timothy.freeman@gmail.com>
2017-08-16 10:37:51 -0400

    done

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,14 @@
+class ProductsController < ApplicationController
+
+	def index
+		@cart = cart
+	end
+
+
+	def add
+		@new_product = params[:product]
+		cart << @new_product
+		redirect_to '/'
+	end
+
+end
